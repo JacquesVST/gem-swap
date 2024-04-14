87389de8c5453ebf143018e62cb5d37769d9db1b
Jacques <jacquesvst@gmail.com>
2024-04-14 01:17:45 -0300

    player, enemy, stage logic

--- sketch.js
+++ sketch.js
@@ -7,45 +7,247 @@ let comboCounter = document.getElementById('comboCounter');
 let bestComboCounter = document.getElementById('bestComboCounter');
 let moveCounter = document.getElementById('moveCounter');
 
-let score = 0;
 let bestScore = 0;
-let combo = 0;
 let bestCombo = 0;
-let moves = 100;
+let uiBarCount = 6
 
 let stackCombo = false;
 let initialShuffle = true;
 
+let globalRun;
 let globalGrid;
 let globalCanvas;
 
-let possibleColors = [
-  [231, 76, 60],  //red
-  [46, 204, 113], //green
-  [46, 134, 193], //blue
-  [244, 208, 63], //yellow
-  [243, 156, 18], //orange
-  [240, 98, 146], //pink
-]
+class ProgressBar {
+  constructor(maxValue, value, title, color) {
+    this.maxValue = maxValue,
+      this.value = value,
+      this.title = title,
+      this.color = color
+  }
+}
+
+class Character {
+  constructor(health) {
+    this.health = health
+    this.currentHealth = health;
+  }
+
+  damage = function (damage) {
+    this.currentHealth -= damage;
+    if (this.currentHealth <= 0) {
+      alert('YOU LOST!')
+      setupGame()
+    }
+  }
+}
+
+class Run {
+  constructor(character, totalFloors, stagesPerFloor, enemyPerStage, movesPerStage) {
+    this.character = character;
+    this.totalFloors = totalFloors;
+    this.stagesPerFloor = stagesPerFloor;
+    this.enemyPerStage = enemyPerStage;
+    this.movesPerStage = movesPerStage
+
+    this.moves = movesPerStage
+    this.score = 0
+    this.combo = 0
+    this.currentFloorIndex = 0;
+    this.defeatedEnemies = 0;
+    this.winState = false;
+
+    this.targetedEnemies = undefined;
+    this.possibleColors = [
+      [231, 76, 60],  //red
+      [46, 204, 113], //green
+      [46, 134, 193], //blue
+      [244, 208, 63], //yellow
+      [243, 156, 18], //orange
+      [240, 98, 146], //pink
+    ]
+
+    this.floors = [...Array(this.totalFloors)].map(
+      (floor, index) => {
+        return new Floor(index + 1, { ...this })
+      }
+    )
+  }
+
+  findFloor = function () {
+    return this.floors[this.currentFloorIndex]
+  }
+
+  findStage = function () {
+    let stageIndex = this.findFloor().currentStageIndex;
+    return this.floors[this.currentFloorIndex].stages[stageIndex]
+  }
+
+  findEnemy = function () {
+    let stageIndex = this.findFloor().currentStageIndex;
+    let enemyIndex = this.findStage().currentEnemyIndex;
+    return this.floors[this.currentFloorIndex].stages[stageIndex].enemies[enemyIndex];
+  }
+
+  checkUpdateProgress = function () {
+    let enemy = this.findEnemy();
+    let stage = this.findStage();
+    let floor = this.findFloor();
+
+    if (enemy.currentHealth <= 0) {
+
+      if (floor.number < this.totalFloors) {
+        if (stage.number < this.stagesPerFloor) {
+          if (enemy.number < this.enemyPerStage) {
+            stage.currentEnemyIndex++
+            this.defeatedEnemies++
+          } else {
+            floor.currentStageIndex++
+          }
+        } else {
+          this.currentFloorIndex++
+        }
+      } else {
+        this.winState = true
+      }
+    }
+  }
+
+  drawRunInfo = function (canvas) {
+    let floor = this.findFloor()
+    let stage = this.findStage();
+    let enemy = this.findEnemy();
+
+    let totalEnemies = this.totalFloors * this.stagesPerFloor * this.enemyPerStage
+
+    let runInfo = [
+      new ProgressBar(
+        totalEnemies,
+        this.defeatedEnemies + 1,
+        'Game',
+        [227, 227, 227]
+      ),
+      new ProgressBar(
+        this.totalFloors,
+        floor.number,
+        'Floor',
+        [236, 200, 19]
+      ),
+      new ProgressBar(
+        this.stagesPerFloor,
+        stage.number,
+        'Floor',
+        [49, 102, 214]
+      ),
+      new ProgressBar(
+        this.enemyPerStage,
+        enemy.number,
+        'Stage',
+        [122, 214, 49]
+      ),
+      new ProgressBar(
+        enemy.health,
+        enemy.currentHealth - 1,
+        'Enemy',
+        [87, 49, 214]
+      ),
+      new ProgressBar(
+        this.character.health,
+        this.character.currentHealth - 1,
+        'Health',
+        [214, 87, 49]
+      ),
+    ]
+
+    runInfo.forEach((element, index) => {
+      drawBar(element, index, canvas)
+    })
+  }
+}
+
+class Floor {
+  constructor(number, run) {
+    this.number = number;
+    this.run = run
+    this.currentStageIndex = 0;
+
+    this.stages = [...Array(this.run.stagesPerFloor)].map(
+      (stage, index) => {
+        return new Stage(index + 1, { ...this })
+      }
+    )
+  }
+}
+
+class Stage {
+  constructor(number, floor) {
+    this.number = number
+    this.floor = floor
+    this.currentEnemyIndex = 0;
+
+    this.enemies = [...Array(this.floor.run.enemyPerStage)].map(
+      (enemy, index) => {
+        let bossMultiplier = 1
+
+        let isBoss = index === this.floor.run.enemyPerStage - 1
+        if (isBoss) {
+          bossMultiplier = 2 * this.floor.number
+        }
+
+        let maxHealth = 1500 * bossMultiplier * (1 + ((this.floor.number - 1) / 2))
+        let minHealth = 500 * bossMultiplier * (1 + ((this.floor.number - 1) / 2))
+
+        let enemyBaseHealth = Math.floor(Math.random() * (maxHealth - minHealth + 1) + minHealth)
+        let enemyBaseAttack = 10 * bossMultiplier
+
+        let enemyTotalHealth = enemyBaseHealth * (1 + ((this.floor.number - 1) / 10)) * (1 + ((this.number - 1) / 100))
+        let enemyTotalAttack = enemyBaseAttack * (1 + ((this.floor.number - 1) / 10)) * (1 + ((this.number - 1) / 100))
+        return new Enemy(index + 1, enemyTotalHealth, enemyTotalAttack, isBoss ? 'Boss' : 'Enemy', { ...this })
+      }
+    )
+  }
+}
+
+class Enemy {
+  constructor(number, health, attack, type, stage) {
+    this.number = number
+    this.health = health
+    this.currentHealth = health;
+    this.attack = attack
+    this.type = type
+    this.stage = stage
+  }
+
+  damage = function (damage, run) {
+    this.currentHealth -= damage;
+    run.checkUpdateProgress()
+  }
+}
 
 class CanvasInfo {
-  constructor(margin, padding, radius) {
+  constructor(margin, padding, radius, uiBarSize) {
     this.margin = margin
     this.padding = padding
     this.radius = radius
+    this.uiBarSize = uiBarSize
+    this.totalUiSize = 0
 
     this.playfieldBackground = 20
+    this.calculatrTotalUiSize();
     this.calculateCanvasAndPlayfield();
   }
 
   calculateCanvasAndPlayfield = function () {
     let screenWidth = document.body.clientWidth;
-    let screenHeight = (screenWidth / 16) * 9;
+    let screenHeight = ((screenWidth / 16) * 9) + this.totalUiSize;
 
     this.canvasSize = new Position(screenWidth, screenHeight)
-    this.playfield = new Position(this.canvasSize.x - 2 * this.margin, this.canvasSize.y - 2 * this.margin)
+    this.playfield = new Position(this.canvasSize.x - 2 * this.margin, (this.canvasSize.y - 2 * this.margin))
     createCanvas(this.canvasSize.x, this.canvasSize.y);
+  }
 
+  calculatrTotalUiSize = function () {
+    this.totalUiSize = ((uiBarCount + 1) * this.margin) + (uiBarCount * this.uiBarSize);
   }
 
   drawPlayfield = function () {
@@ -85,7 +287,7 @@ class Item {
     this.id = generateId();
   }
 
-  static generateRandomItem = function (position, gridSideSize, possibleShapes = possibleColors) {
+  static generateRandomItem = function (position, gridSideSize, possibleShapes) {
     let randomShape = Math.floor(Math.random() * possibleShapes.length);
     let shape = new Shape(randomShape + 3, possibleShapes[randomShape])
     return new Item(shape, position, gridSideSize / 3);
@@ -133,12 +335,13 @@ class Grid {
     do {
       this.sideSize++;
       this.horizontalCenterPadding = canvas.playfield.x - (this.width * this.sideSize) - (this.width * canvas.padding) - canvas.padding
-      this.verticalCenterPadding = canvas.playfield.y - (this.height * this.sideSize) - (this.height * canvas.padding) - canvas.padding
+      this.verticalCenterPadding = canvas.playfield.y - canvas.totalUiSize - (this.height * this.sideSize) - (this.height * canvas.padding) - canvas.padding
     } while (this.horizontalCenterPadding - this.width >= 0 && this.verticalCenterPadding - this.height >= 0)
 
     this.iterateXtoY((x, y) => {
+
       let currentXMargin = (this.horizontalCenterPadding / 2) + (x * this.sideSize) + (x * canvas.padding) + canvas.padding + canvas.margin;
-      let currentYMargin = (this.verticalCenterPadding / 2) + (y * this.sideSize) + (y * canvas.padding) + canvas.padding + canvas.margin;
+      let currentYMargin = canvas.totalUiSize + (this.verticalCenterPadding / 2) + (y * this.sideSize) + (y * canvas.padding) + canvas.padding + canvas.margin;
 
       this.cells[x][y].canvasPosition = new Position(currentXMargin, currentYMargin);
     });
@@ -148,11 +351,11 @@ class Grid {
     return [...this.cells.flat()].every(cell => cell.item);
   }
 
-  generateItems = function () {
+  generateItems = function (run) {
     this.iterateYtoX((x, y) => {
       let cell = this.getCellbyPosition(new Position(x, y));
       if (!cell.item && cell.position.y === 0) {
-        this.cells[x][y].item = Item.generateRandomItem(this.cells[x][y].position, this.sideSize)
+        this.cells[x][y].item = Item.generateRandomItem(this.cells[x][y].position, this.sideSize, run.possibleColors)
       }
     })
   }
@@ -206,11 +409,11 @@ class Grid {
     this.getCellbyPosition(position2).item = item1 ? item1.renewPosition(position2) : undefined;
 
     if (humanSwap) {
-      updateMoves(moves - 1);
+      updateMoves(globalRun.moves - 1);
     }
   }
 
-  draw = function (canvas) { //ok
+  draw = function (canvas) {
     this.cells.flat().forEach(cell => {
       noStroke()
       let limits = [
@@ -237,7 +440,7 @@ class Grid {
     })
   }
 
-  drawItems = function () { //ok
+  drawItems = function (canvas) { //ok
     this.cells.flat().map(cell => cell.item).forEach(item => {
       if (item) {
         let cellRef = this.getCellbyPosition(item.position);
@@ -276,27 +479,6 @@ class Grid {
 
 }
 
-function canReach(pos1, pos2) {
-  if (pos1.x === pos2.x) {
-    return [pos1.y - 1, pos1.y + 1].includes(pos2.y);
-  } else if (pos1.y === pos2.y) {
-    return [pos1.x - 1, pos1.x + 1].includes(pos2.x);
-  }
-  return false
-
-}
-
-function polygon(x, y, radius, npoints) {
-  let angle = TWO_PI / npoints;
-  beginShape();
-  for (let a = HALF_PI * 3; a < HALF_PI * 7; a += angle) {
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
-
 function setup() {
   gridInputX.value = 7
   gridInputY.value = 5
@@ -310,42 +492,40 @@ function setup() {
 
 function setupGame() {
   setTimeout(() => {
-    globalCanvas = new CanvasInfo(16, 4, 4);
+    let character = new Character(1000)
+    globalRun = new Run(character, 3, 3, 3, 15);
+    globalCanvas = new CanvasInfo(16, 4, 4, 20);
     globalGrid = new Grid(parseInt(gridInputX.value, 10), parseInt(gridInputY.value, 10));
     globalGrid.calculateSpacing(globalCanvas);
 
     updateScore([], true)
     updateCombo([], true)
-    updateMoves(15)
-
-    if (!isLooping()) {
-      loop()
-    }
+    updateMoves(globalRun.movesPerStage)
   }, 0);
 
 }
 
 function draw() {
-
   removeMatches(findMatches(globalGrid));
 
   if (!globalGrid.isFull()) {
     applyGravity(globalGrid);
-    globalGrid.generateItems();
+    globalGrid.generateItems(globalRun);
   } else {
-    if (moves === 0) {
-
-      if (confirm('YOU LOST! RESET ?')) {
-        setupGame();
-      } else {
-        noLoop();
-      }
+    if (globalRun.moves === 0) {
+      reload()
     }
   }
 
   globalCanvas.drawPlayfield();
+  globalRun.drawRunInfo(globalCanvas)
   globalGrid.draw(globalCanvas);
-  globalGrid.drawItems();
+  globalGrid.drawItems(globalCanvas);
+
+  if (globalRun.winState) {
+    alert('YOU WON!')
+    setupGame();
+  }
 }
 
 function mouseClicked(event) {
@@ -369,7 +549,7 @@ function mouseClicked(event) {
         if (!globalGrid.selectedCellPos) {
           globalGrid.selectedCellPos = cell.position
           stackCombo = false;
-          combo = 0
+          globalRun.combo = 0
         } else {
           stackCombo = true;
           globalGrid.swap(cell.position, globalGrid.selectedCellPos, true);
@@ -380,7 +560,6 @@ function mouseClicked(event) {
     }, () => clickFound)
   }
 }
-
 
 function findMatches(grid) {
   let matches = [];
@@ -452,50 +631,72 @@ function applyGravity(grid) {
   });
 }
 
-function sleep(millisecondsDuration) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, millisecondsDuration);
-  })
+function canReach(pos1, pos2) {
+  if (pos1.x === pos2.x) {
+    return [pos1.y - 1, pos1.y + 1].includes(pos2.y);
+  } else if (pos1.y === pos2.y) {
+    return [pos1.x - 1, pos1.x + 1].includes(pos2.x);
+  }
+  return false
+
+}
+
+function reload() {
+  updateMoves(globalRun.movesPerStage);
+  globalRun.character.damage(globalRun.findEnemy().attack)
+}
+
+function damageEnemy(run, damage) {
+  let enemy = run.findEnemy()
+
+  if (enemy.currentHealth < damage) {
+    damage = enemy.currentHealth;
+  }
+
+  enemy.damage(damage, run)
 }
 
 function updateScore(match, resetCounter = false) {
   if (resetCounter) {
-    score = 0
+    globalRun.score = 0
     initialShuffle = true;
   }
 
-  score += 100 * match.length * combo;
-  scoreCounter.innerHTML = score
+  let additiveScore = 100 * match.length * globalRun.combo;
 
-  bestScore = bestScore > score ? bestScore : score;
+  globalRun.score += additiveScore
+  scoreCounter.innerHTML = globalRun.score
+
+  bestScore = bestScore > globalRun.score ? bestScore : globalRun.score;
   bestScoreCounter.innerHTML = bestScore
+
+  damageEnemy(globalRun, additiveScore);
 }
 
 function updateCombo(matches, resetCounter = false) {
   if (resetCounter) {
-    combo = 0
+    globalRun.combo = 0
     initialShuffle = true;
     stackCombo = false;
     comboCounter.style = 'font-size: 1em'
   }
 
-  combo += [...matches].length;
-  comboCounter.innerHTML = combo
+  globalRun.combo += [...matches].length;
+  comboCounter.innerHTML = globalRun.combo
 
-  bestCombo = bestCombo > combo ? bestCombo : combo;
+  bestCombo = bestCombo > globalRun.combo ? bestCombo : globalRun.combo;
   bestComboCounter.innerHTML = bestCombo
 
-  let fontSize = combo > 0 ? combo : 1;
+  let fontSize = globalRun.combo > 0 ? globalRun.combo : 1;
   comboCounter.style = 'font-size: ' + fontSize + 'em'
 }
 
 function updateMoves(value) {
-  moves = value
+  globalRun.moves = value
 
-  moveCounter.innerHTML = moves
+  moveCounter.innerHTML = globalRun.moves
   moveCounter.style = value > 10 ? 'color: white' : 'color: red';
 }
-
 
 function checkMouseOver(minX, maxX, minY, maxY) {
   return mouseX > minX && mouseX < maxX && mouseY > minY && mouseY < maxY;
@@ -503,6 +704,37 @@ function checkMouseOver(minX, maxX, minY, maxY) {
 
 function checkPositionInLimit(minX, maxX, minY, maxY, pos) {
   return pos.x > minX && pos.x < maxX && pos.y > minY && pos.y < maxY;
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = HALF_PI * 3; a < HALF_PI * 7; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function drawBar(element, index, canvas) {
+  let percentageOfBar = element.value / element.maxValue
+
+  noStroke()
+  fill(percentageOfBar ? element.color : [20, 20, 20]);
+  rect(
+    canvas.margin + canvas.padding,
+    (canvas.margin * (index + 2)) + (canvas.uiBarSize * index),
+    (canvas.playfield.x - (2 * canvas.padding)) * percentageOfBar,
+    canvas.uiBarSize,
+    canvas.radius + canvas.padding
+  );
+}
+
+function sleep(millisecondsDuration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecondsDuration);
+  })
 }
 
 function generateId() {
